06eb6bf60d9ae9cbda20eac48acd1b43a34646e0
Kholiq <kholiqcode@gmail.com>
2020-09-05 04:06:04 +0700

    revisi tutor model

--- application/models/api/Tutor_model.php
+++ application/models/api/Tutor_model.php
@@ -52,11 +52,11 @@ class Tutor_model extends CI_Model
     {
 
         if (isset($input['tid']) && !empty($input['tid'])) {
-            $query    = $this->db->select('tutor.*,users.telepon')->where('tutor.id', $input['tid'])->join('users', 'tutor.id_users=users.id')->get($this->table)->row_array();
+            $query    = $this->db->select('tutor.*,users.telepon,users.jenis_kelamin')->where('tutor.id', $input['tid'])->join('users', 'tutor.id_users=users.id')->get($this->table)->row_array();
         } else if (isset($input['kid']) && !empty($input['kid'])) {
-            $query    = $this->db->select('tutor.*,users.telepon')->where('tutor.id_kategori', $input['kid'])->join('users', 'tutor.id_users=users.id')->get($this->table)->result_array();
+            $query    = $this->db->select('tutor.*,users.telepon,users.jenis_kelamin')->where('tutor.id_kategori', $input['kid'])->join('users', 'tutor.id_users=users.id')->get($this->table)->result_array();
         } else {
-            $query    = $this->db->select('tutor.*,users.telepon')->join('users', 'tutor.id_users=users.id')->get($this->table)->result_array();
+            $query    = $this->db->select('tutor.*,users.telepon,users.jenis_kelamin')->join('users', 'tutor.id_users=users.id')->get($this->table)->result_array();
         }
 
         return $query;
