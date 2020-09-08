934b779ce3e59f14c445a18c0f6d8a9fbf519c34
Kholiq <kholiqcode@gmail.com>
2020-09-08 10:21:53 +0700

    rev ubah password

--- application/controllers/api/Users.php
+++ application/controllers/api/Users.php
@@ -58,6 +58,56 @@ class Users extends RestController {
         }
     }
 
+    public function ubah_password_put()
+    {
+        $input = $this->put();
+
+        
+        if (!$this->validateUbahPassword($input) || !$this->user->verifyPasswordLama($input)) {
+            $this->response([
+                'status' => false,
+                'message' => validation_errors(null, null)
+            ], 404);
+        }
+
+        if ($this->user->ubahPasswordUsers($input) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'Password berhasil diperbarui'
+            ], 200);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Terjadi kesalahan'
+            ], 404);
+        }
+    }
+
+    public function validateUbahPassword($input)
+    {
+        $validationRules = [
+            [
+                'field' => 'password_lama',
+                'label'    => 'Password Lama',
+                'rules'    => 'required|min_length[5]',
+            ],
+            [
+                'field' => 'password_baru',
+                'label'    => 'Password Baru',
+                'rules'    => 'required|min_length[6]',
+            ],
+            [
+                'field' => 'konfirmasi_password',
+                'label'    => 'Konfirmasi Password Baru',
+                'rules'    => 'required|matches[password_baru]',
+            ]
+        ];
+
+        $res = validateReq($validationRules, $input);
+
+        return $res;
+    }
+
     public function validatePut($input)
     {
         $validationRules = [
--- application/models/api/Users_model.php
+++ application/models/api/Users_model.php
@@ -5,22 +5,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Users_model extends CI_Model
 {
 
-	protected $table = 'users';
+    protected $table = 'users';
 
-	public function getUsers($input = null)
-	{
-		$uid = $this->token->decrypt($input['apikey']);
+    public function getUsers($input = null)
+    {
+        $uid = $this->token->decrypt($input['apikey']);
 
-		return $this->db->select('id,nama,email,jenis_kelamin,telepon,alamat,foto')->where('id', $uid)->get($this->table)->result_array();
-	}
+        return $this->db->select('id,nama,email,jenis_kelamin,telepon,alamat,foto')->where('id', $uid)->get($this->table)->result_array();
+    }
 
-	public function putUsers($input)
+    public function putUsers($input)
     {
         $uid = $this->token->decrypt($input['apikey']);
 
         $data        = [
-			'nama'        => $input['nama'],
-			'jenis_kelamin'    => $input['jenis_kelamin'],
+            'nama'        => $input['nama'],
+            'jenis_kelamin'    => $input['jenis_kelamin'],
             'alamat'    => $input['alamat'],
             'telepon'        => $input['telepon'],
         ];
@@ -28,6 +28,33 @@ class Users_model extends CI_Model
         $this->db->where('id', $uid)->update($this->table, $data);
 
         return $this->db->affected_rows();
+    }
+
+    public function ubahPasswordUsers($input)
+    {
+        $uid = $this->token->decrypt($input['apikey']);
+
+        $data        = [
+            'password'        => password_hash($input['password_baru'], PASSWORD_BCRYPT)
+        ];
+
+        $this->db->where('id', $uid)->update($this->table, $data);
+
+        return $this->db->affected_rows();
+    }
+
+    public function verifyPasswordLama($input)
+    {
+        $uid = $this->token->decrypt($input['apikey']);
+
+        $query = $this->db->select('password')->where('id', $uid)->get($this->table)->row_array();
+
+        if (is_array($query) && password_verify($input['password_lama'], $query['password'])) {
+            // var_dump($query);
+            return true;
+        }
+
+        return false;
     }
 }
 
